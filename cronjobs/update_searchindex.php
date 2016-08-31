62d6a44621fb03b4bdc59a9d2728004aacff93ef
fishnet Admin <klaporta@eileenfisher.com>
2016-08-31 12:28:29 -0400

    search update
    Tags are now included in the search.

--- cronjobs/update_searchindex.php
+++ cronjobs/update_searchindex.php
@@ -18,7 +18,7 @@ $db->beginTransaction();
 //this query might take a while
 $yesterday = date("Y-m-d", strtotime("-1 week"));
 $query = $db->query("
-			SELECT rev.*, fn_pages.title, fn_pages.date_published, rel.section_id, sections.title as section_title
+			SELECT rev.*, fn_pages.title, fn_pages.date_published, rel.section_id, sections.title as section_title,  fn_tags.tag_id, fn_tags.tag_name, fn_permissions.access
 			FROM (SELECT * FROM (
 					SELECT r.page_id, r.revision_text
 					FROM fn_revisions r
@@ -27,6 +27,9 @@ $query = $db->query("
 				GROUP BY rev1.page_id) rev
 			JOIN fn_pages ON rev.page_id=fn_pages.page_id
 			JOIN fn_templates ON fn_pages.template_id=fn_templates.template_id
+			JOIN fn_tag_matches ON rev.page_id=fn_tag_matches.page_id # added by Frank!
+			JOIN fn_tags ON fn_tag_matches.tag_id=fn_tags.tag_id # added by Frank!
+			JOIN fn_permissions ON fn_permissions.page_id=rev.page_id # added by Frank!
 			LEFT JOIN fn_pages_pages rel ON rel.page_id=fn_pages.page_id
 			LEFT JOIN fn_pages sections on rel.section_id=sections.page_id
 			WHERE fn_pages.published=1 AND fn_pages.deleted=0 AND fn_templates.page_type<>'system';
@@ -54,18 +57,21 @@ if ($len > 0){
 			$value .= $db->quote($row['date_published']).",";
 			$value .= $db->quote($row['section_id']).",";
 			$value .= $db->quote($row['section_title']);
+			$value .= $db->quote($row['tag_id']).","; // Added by Frank!
+			$value .= $db->quote($row['tag_name']).","; // Added by Frank!
+			$value .= $db->quote($row['access']); // Added by Frank!
 			$value .= ")";
 
 			$insertValues[] = $value;
 
 			if (($i != 0) and (($i % 50) == 0) or ($i == ($len - 1))){
 				$strInsertQuery = "INSERT INTO fn_searchindex(obj_id, obj_type, page_title, page_content,
-								page_date_published, section_id, section_title) VALUES ";
-				$strInsertQuery .= implode(",", $insertValues);
-				$strInsertQuery .= " ON DUPLICATE KEY UPDATE obj_id=VALUES(obj_id), obj_type=VALUES(obj_type),
+								page_date_published, section_id, section_title, tag_id, tag_name, access) VALUES ";
+						$strInsertQuery .= implode(",", $insertValues);
+						$strInsertQuery .= " ON DUPLICATE KEY UPDATE obj_id=VALUES(obj_id), obj_type=VALUES(obj_type),
 								page_title=VALUES(page_title), page_content=VALUES(page_content),
 								page_date_published=VALUES(page_date_published), section_id=VALUES(section_id),
-								section_title=VALUES(section_title)";
+								section_title=VALUES(section_title), tag_id=VALUES(tag_id), tag_name=VALUES(tag_name), access=VALUES(access)";
 
 				//echo "$i of ".($len - 1)."<br><br>";
 				$ar = $db->exec($strInsertQuery);
